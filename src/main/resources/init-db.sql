81ff6ee55c86b455593b78415fbaf075b226da77
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-28 20:03:06 +0500

    update sql script: add unique for target and base currencies

--- src/main/resources/init-db.sql
+++ src/main/resources/init-db.sql
@@ -10,6 +10,7 @@ CREATE TABLE exchange_rate (
                                base_currency_id INTEGER,
                                target_currency_id INTEGER,
                                rate DECIMAL(9, 6) NOT NULL,
+                               UNIQUE (base_currency_id, target_currency_id),
                                FOREIGN KEY (base_currency_id) REFERENCES currency(id),
                                FOREIGN KEY (target_currency_id) REFERENCES currency(id)
 );
