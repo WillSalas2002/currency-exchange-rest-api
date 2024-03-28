d527634ea7233ba967b12f9786bfa2274a5924c7
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-28 17:22:19 +0500

    create model entities

---
+++ src/main/java/com/will/currency/exchange/api/model/Currency.java
@@ -0,0 +1,15 @@
+package com.will.currency.exchange.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Currency {
+    private Integer id;
+    private String code;
+    private String fullName;
+    private String sign;
+}
