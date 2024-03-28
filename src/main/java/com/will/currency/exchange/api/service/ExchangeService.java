f2b2ecb2478392ae3373d3e9e8bc8a1ae6ea21a2
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-28 20:38:29 +0500

    Create ExchangeService: implement direct logic, when there in the database the exchange rate is stored directly

--- src/main/java/com/will/currency/exchange/api/service/ExchangeService.java
+++ src/main/java/com/will/currency/exchange/api/service/ExchangeService.java
@@ -1,13 +1,25 @@
 package com.will.currency.exchange.api.service;
 
+import com.will.currency.exchange.api.dto.ExchangeRateDto;
+import com.will.currency.exchange.api.model.ExchangeRate;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExchangeService {
     private static final ExchangeService INSTANCE = new ExchangeService();
 
-
+    public ExchangeRateDto calculateDirectExchange(ExchangeRate exchangeRate, BigDecimal amount) {
+        return new ExchangeRateDto(
+                exchangeRate.getBaseCurrency(),
+                exchangeRate.getTargetCurrency(),
+                exchangeRate.getRate(),
+                amount,
+                exchangeRate.getRate().multiply(amount)
+        );
+    }
 
     public static ExchangeService getINSTANCE() {
         return INSTANCE;
