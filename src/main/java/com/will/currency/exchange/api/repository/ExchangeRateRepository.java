7e692ce94ca38237d3bbeddb368202edfcfd803e
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-28 18:07:07 +0500

    ExchangeRateRepository class: wrote logic for fetching exchange rate by currency codes

--- src/main/java/com/will/currency/exchange/api/repository/ExchangeRateRepository.java
+++ src/main/java/com/will/currency/exchange/api/repository/ExchangeRateRepository.java
@@ -12,6 +12,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExchangeRateRepository {
@@ -29,7 +30,11 @@ public class ExchangeRateRepository {
                    er.rate AS rate
             FROM exchange_rate er
                      JOIN currency c_base ON er.base_currency_id = c_base.id
-                     JOIN currency c_target ON er.target_currency_id = c_target.id;
+                     JOIN currency c_target ON er.target_currency_id = c_target.id
+            """;
+
+    private final static String FIND_BY_CURRENCY_CODES_SQL = FIND_ALL_SQL + """
+            WHERE c_base_code = ? AND c_target_code = ?;
             """;
 
     public List<ExchangeRate> findAll() {
@@ -38,30 +43,52 @@ public class ExchangeRateRepository {
             List<ExchangeRate> exchangeRates = new ArrayList<>();
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                Currency baseCurrency = new Currency(
-                        resultSet.getInt("c_base_id"),
-                        resultSet.getString("c_base_code"),
-                        resultSet.getString("c_base_full_name"),
-                        resultSet.getString("c_base_sign")
-                );
-                Currency targetCurrency = new Currency(
-                        resultSet.getInt("c_target_id"),
-                        resultSet.getString("c_target_code"),
-                        resultSet.getString("c_target_full_name"),
-                        resultSet.getString("c_target_sign")
-                );
-                exchangeRates.add(
-                        new ExchangeRate(
-                                resultSet.getInt("id"),
-                                baseCurrency,
-                                targetCurrency,
-                                resultSet.getBigDecimal("rate")
-                        ));
+                exchangeRates.add(getExchangeRate(resultSet));
             }
             return exchangeRates;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Optional<ExchangeRate> findByCurrencyCodes(String baseCurrencyCode, String targetCurrencyCode) {
+        try (Connection connection = ConnectionManager.get();
+             PreparedStatement statement = connection.prepareStatement(FIND_BY_CURRENCY_CODES_SQL)) {
+            ExchangeRate exchangeRate = null;
+            statement.setString(1, baseCurrencyCode);
+            statement.setString(2, targetCurrencyCode);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                exchangeRate = getExchangeRate(resultSet);
+            }
+            return Optional.ofNullable(exchangeRate);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ExchangeRate getExchangeRate(ResultSet resultSet) throws SQLException {
+        ExchangeRate exchangeRate;
+        Currency baseCurrency = new Currency(
+                resultSet.getInt("c_base_id"),
+                resultSet.getString("c_base_code"),
+                resultSet.getString("c_base_full_name"),
+                resultSet.getString("c_base_sign")
+        );
+        Currency targetCurrency = new Currency(
+                resultSet.getInt("c_target_id"),
+                resultSet.getString("c_target_code"),
+                resultSet.getString("c_target_full_name"),
+                resultSet.getString("c_target_sign")
+        );
+        exchangeRate = (
+                new ExchangeRate(
+                        resultSet.getInt("id"),
+                        baseCurrency,
+                        targetCurrency,
+                        resultSet.getBigDecimal("rate")
+                ));
+        return exchangeRate;
     }
 
     public static ExchangeRateRepository getINSTANCE() {
