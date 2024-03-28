9040081597436cc9bacfa32d74c9b2fb77a0874e
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-28 17:28:42 +0500

    Create database init script file

---
+++ src/main/resources/init-db.sql
@@ -0,0 +1,15 @@
+CREATE TABLE currency (
+                          id INTEGER PRIMARY KEY ,
+                          code VARCHAR(50) UNIQUE NOT NULL,
+                          full_name VARCHAR(150) NOT NULL,
+                          sign VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE exchange_rate (
+                               id INTEGER PRIMARY KEY AUTOINCREMENT,
+                               base_currency_id INTEGER,
+                               target_currency_id INTEGER,
+                               rate DECIMAL(9, 6) NOT NULL,
+                               FOREIGN KEY (base_currency_id) REFERENCES currency(id),
+                               FOREIGN KEY (target_currency_id) REFERENCES currency(id)
+);
