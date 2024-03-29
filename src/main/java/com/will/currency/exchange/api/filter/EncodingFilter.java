1c915d60b462ec531565b93e4ed9b3c1eba6d688
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-29 17:06:55 +0500

    ExchangeService: implement usd based logic for calculating exchange

---
+++ src/main/java/com/will/currency/exchange/api/filter/EncodingFilter.java
@@ -0,0 +1,22 @@
+package com.will.currency.exchange.api.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setContentType("application/json");
+        System.out.println(StandardCharsets.UTF_8.name());
+        response.setContentType(StandardCharsets.UTF_8.name());
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/will/currency/exchange/api/servlet/CurrencyServlet.java
@@ -0,0 +1,8 @@
+package com.will.currency.exchange.api.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/currency/*")
+public class CurrencyServlet extends HttpServlet {
+}
