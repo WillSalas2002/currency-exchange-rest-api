8a0f4ab0062c87f2fe2005b8f913895f718725ea
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-29 17:07:39 +0500

    Revert "ExchangeService: implement usd based logic for calculating exchange"
    This reverts commit 1c915d60b462ec531565b93e4ed9b3c1eba6d688.

--- src/main/java/com/will/currency/exchange/api/servlet/CurrencyServlet.java
+++
@@ -1,8 +0,0 @@
-package com.will.currency.exchange.api.servlet;
-
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-
-@WebServlet("/currency/*")
-public class CurrencyServlet extends HttpServlet {
-}
