80c0a5f326c9642ec17e97a656f88c5f0e2bbb63
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-29 21:04:34 +0500

    Update CurrenciesServlet: implement doGet() method

--- src/main/java/com/will/currency/exchange/api/servlet/CurrenciesServlet.java
+++ src/main/java/com/will/currency/exchange/api/servlet/CurrenciesServlet.java
@@ -1,5 +1,8 @@
 package com.will.currency.exchange.api.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.will.currency.exchange.api.dto.CurrencyDto;
+import com.will.currency.exchange.api.service.CurrencyService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,12 +10,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/currencies")
 public class CurrenciesServlet extends HttpServlet {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final CurrencyService currencyService = CurrencyService.getInstance();
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // TODO: Exception handling!!!
+        List<CurrencyDto> currencyDtos = currencyService.findAll();
+        resp.setStatus(HttpServletResponse.SC_OK);
+        objectMapper.writeValue(resp.getWriter(), currencyDtos);
     }
 
     @Override
