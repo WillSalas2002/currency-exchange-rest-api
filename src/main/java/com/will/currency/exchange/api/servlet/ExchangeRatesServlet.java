b2ec9e5a4e0799e86149e46d76832a0f1877d39d
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-29 22:07:56 +0500

    Update ExchangeRatesServlet.java: implement doGet method

--- src/main/java/com/will/currency/exchange/api/servlet/ExchangeRatesServlet.java
+++ src/main/java/com/will/currency/exchange/api/servlet/ExchangeRatesServlet.java
@@ -1,18 +1,26 @@
 package com.will.currency.exchange.api.servlet;
 
-import jakarta.servlet.ServletException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.will.currency.exchange.api.dto.ExchangeRateDto;
+import com.will.currency.exchange.api.service.ExchangeRateService;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/exchangeRates")
 public class ExchangeRatesServlet extends HttpServlet {
+    private final ExchangeRateService exchangeRateService = ExchangeRateService.getInstance();
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        //TODO: exception handling
+        List<ExchangeRateDto> exchangeRateDtoList = exchangeRateService.findAll();
+        resp.setStatus(HttpServletResponse.SC_OK);
+        objectMapper.writeValue(resp.getWriter(), exchangeRateDtoList);
     }
 }
