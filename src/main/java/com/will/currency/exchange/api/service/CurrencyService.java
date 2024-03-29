ae4cfc9b2727a0106a138e42be76bbe9d47a6f6b
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-29 17:32:56 +0500

    CurrencyService.java: Implement findByCurrencyCode() method

--- src/main/java/com/will/currency/exchange/api/service/CurrencyService.java
+++ src/main/java/com/will/currency/exchange/api/service/CurrencyService.java
@@ -1,11 +1,28 @@
 package com.will.currency.exchange.api.service;
 
+import com.will.currency.exchange.api.dto.CurrencyDto;
+import com.will.currency.exchange.api.repository.CurrencyRepository;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CurrencyService {
     private static final CurrencyService INSTANCE = new CurrencyService();
+    private final CurrencyRepository currencyRepository = CurrencyRepository.getINSTANCE();
+
+    public List<CurrencyDto> findByCurrencyCode(String code) {
+        return currencyRepository.findByCurrencyCode(code)
+                .stream()
+                .map(currency -> new CurrencyDto(
+                        currency.getId(),
+                        currency.getCode(),
+                        currency.getFullName(),
+                        currency.getSign()))
+                .collect(Collectors.toList());
+    }
 
     public static CurrencyService getInstance() {
         return INSTANCE;
