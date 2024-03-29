0265d767e02e3e9eaa4c4e82002d24b075131bdb
Ilkham Jumatov <batir2jumatov@gmail.com>
2024-03-29 21:04:51 +0500

    Update CurrencyServlet: implement doGet() method

--- src/main/java/com/will/currency/exchange/api/servlet/CurrencyServlet.java
+++ src/main/java/com/will/currency/exchange/api/servlet/CurrencyServlet.java
@@ -1,5 +1,8 @@
 package com.will.currency.exchange.api.servlet;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.will.currency.exchange.api.dto.CurrencyDto;
+import com.will.currency.exchange.api.service.CurrencyService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -7,12 +10,25 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet("/currency/*")
 public class CurrencyServlet extends HttpServlet {
+    private final CurrencyService currencyService = CurrencyService.getInstance();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        String currencyCode = req.getPathInfo().substring(1).toUpperCase();
+        // TODO: Validation of the currency code and exception handling!!!
+        Optional<CurrencyDto> resultOptional = currencyService.findByCurrencyCode(currencyCode);
+        if (resultOptional.isEmpty()) {
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            objectMapper.writeValue(resp.getWriter(), "Not found");
+            return;
+        }
+        resp.setStatus(HttpServletResponse.SC_OK);
+        objectMapper.writeValue(resp.getWriter(), resultOptional.get());
     }
 
     @Override
